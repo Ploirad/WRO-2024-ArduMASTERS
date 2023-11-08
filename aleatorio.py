42434df062bdf37e6ade1cf0b4ea1d18b24213cd
Ploirad <dlopezp36@gmail.com>
2023-11-08 19:41:09 +0100

    aleatorio

---
+++ aleatorio.py
@@ -0,0 +1,12 @@
+git config --global user.email "dlopezp36@gmail.com"
+git config --global user.name "Ploirad"
+ 
+#importacion de librerias
+
+#funcion principal Mario
+
+#funcion aleatoria Mario
+
+#Comprobacion de resultados Darío
+
+#Fin del juego Adrián
