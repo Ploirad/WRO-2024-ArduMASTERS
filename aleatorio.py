772fe599be4419d9e70c3b521235920548c3d1f3
AdriROBOTICS <adriyabe@gmail.com>
2023-11-12 20:53:28 +0100

    aleatorio.py

--- aleatorio.py
+++ aleatorio.py
@@ -9,3 +9,19 @@
 #Comprobacion de resultados Darío
 
 #Fin del juego Adrián
+texto = input()
+intentos = 0
+aprox = ""
+win = texto == numero
+if win == True:
+    print("HAS GANADO, ¿QUIERES SEGUIR? (SI/NO)")
+    if texto == "SI":
+        #volver a hacer bucle
+    elif texto == "NO":
+        #cancelar repetición
+    else:
+        print("ESCRIBE -SI- O -NO-")
+else:#aprox="GRANDE" // aprox="PEQUEÑO"
+    print(f"NO, ES MÁS {aprox}")
+    #Continuar
+    intentos += 1
