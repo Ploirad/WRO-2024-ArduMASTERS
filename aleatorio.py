3930bdc08615ac554cee4236d481e2b8f50731c0
Ploirad <dlopezp36@gmail.com>
2023-11-15 20:03:39 +0100

    hola

--- aleatorio.py
+++ aleatorio.py
@@ -1,49 +1,33 @@
-
- 
-#importacion de librerias
-import random 
-fin = False
-#fin indica el final de la partida
-intentos = 0
-#intentos son la cantidad de intentos que llevan los jugadores
-distancia = ""
-#distancia es para que la pongas directamente cuando digas la proximidad de el número
-numero = 0
-#numero es lo que los jugadores han insertado
-repeticion = False
-again = True
-#devuelve numero 
+import random
 def aleatorio ():
-    return random.randint(1, 100)
-
-#funcion aleatoria Mario
+   return random.randint(1, 100)
+    
 def main():
+   numero = 0
+   acertado = False
+   num_aleatorio = aleatorio()
+   intentos = 0
    print("Bienvenido a Adivina el número!!!")
-   print("Por favor, introduzca un número:")
-   numero = input()
-#Comprobacion de resultados Darío
-   if numero == aleatorio:
-      fin = True
-   elif numero > aleatorio:
-      distancia = "ALTO"
-   elif numero < aleatorio:
-      distancia = "BAJO"
-#Fin del juego Adrián
-   if fin == True:
-      print("HAS GANADO, ¿QUIERES SEGUIR? (SI/NO)")
-      while repeticion == False:
-         texto = input()
-         if texto == "SI":
-            repeticion = True
-         elif texto == "NO":
-            repeticion = True
-            again = False
-      #cancelar repetición
-         else:
-            print("ESCRIBE -SI- O -NO-")
-   else:#aprox="GRANDE" // aprox="PEQUEÑO"
-      print(f"NO, ES MÁS {distancia}")
-    #Continuar
+   while acertado == False:
+      print("Por favor, introduzca un número:")
+      numero = input()
       intentos += 1
-while again == True:
+      if int(numero) == num_aleatorio:
+         print("Lo has adivinado en " + intentos)
+         print("HAS GANADO, ¿QUIERES SEGUIR? (SI/NO)")
+         while repeticion == False:
+            texto = input()
+            acertado = True
+            if texto == "SI":
+               acertado = 
+            elif texto == "NO":
+               acertado = False
+            else:
+               print("ESCRIBE -SI- O -NO-")
+      elif int(numero) < num_aleatorio:
+         print("NO, ES MÁS ALTO")
+      elif int(numero) > num_aleatorio:
+         print("NO, ES MÁS BAJO")
+
+while hola == True:
    main()
