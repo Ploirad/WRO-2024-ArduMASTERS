6cb164d474f7d393f3b079fa4ace54d600552ec8
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-02-28 20:04:21 +0100

    Update pruebagpio.py

--- src/pruebagpio.py
+++ src/pruebagpio.py
@@ -1,25 +1,45 @@
 import RPi.GPIO as GPIO
 import time
 
+# Limpia los pines GPIO
+GPIO.cleanup()
+
 # Define el pin GPIO que utilizarás para controlar el servo
-servo_pin = 18
+servo_pin_direccion = 18
+servo_pin_traccion = 17
+boton_pin = 27
+
+valor_d = 7.5 #2.5=izq; 7.5=centro; 12.5=der
+valor_t = 0   #6=Atras;  8=Alante;  =stop
 
 # Configura el modo de los pines GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(servo_pin, GPIO.OUT)
+GPIO.setup(servo_pin_direccion, GPIO.OUT)
+GPIO.setup(servo_pin_traccion, GPIO.OUT)
+GPIO.setup(boton_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Crea un objeto PWM (Modulación de Ancho de Pulso) en el pin seleccionado
-pwm = GPIO.PWM(servo_pin, 50) # Frecuencia de PWM: 50Hz (estándar para servos)
+pwm_d = GPIO.PWM(servo_pin_direccion, 50) # Frecuencia de PWM: 50Hz (estándar para servos)
+pwm_t = GPIO.PWM(servo_pin_traccion, 50) # Frecuencia de PWM: 50Hz (estándar para servos)
 
-# Inicializa el servo en su posición neutra (90 grados)
-pwm.start(7) # Puede necesitar ajustar este valor según tu servo
-#6=Atras;  8=Alante;  =stop
-#2.5=izq; 7.5; 12,5=der
-# Espera un tiempo para que el servo se mueva a la posición inicial
-time.sleep(1)
+while True:
+  arranco = GPIO.input(boton_pin)
+  if arranco == GPIO.LOW:
+    print("ON"))
+    valor_d = 7.5
+    valor_t = 8
+    pwm_d.start(valor_d)
+    pwm_t.start(valor_t)
+    
+    valor_d = 2.5
+    valor_t = 6
+    pwm_d.start(valor_d)
+    pwm_t.start(valor_t)
+    
+    valor_d = 12.5
+    valor_t = 8
+    pwm_d.start(valor_d)
+    pwm_t.start(valor_t)
 
 # Detén el servo (opcional)
 pwm.stop()
-
-# Limpia los pines GPIO
-GPIO.cleanup()
