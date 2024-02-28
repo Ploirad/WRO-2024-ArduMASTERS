3fbfa25f0f9ad81c1e77ef362b43ec7e94492c96
Ploirad <dlopezp36@gmail.com>
2024-02-28 18:13:30 +0000

    Updated prubagpio.py

--- src/pruebagpio.py
+++ src/pruebagpio.py
@@ -12,10 +12,10 @@ GPIO.setup(servo_pin, GPIO.OUT)
 pwm = GPIO.PWM(servo_pin, 50) # Frecuencia de PWM: 50Hz (estándar para servos)
 
 # Inicializa el servo en su posición neutra (90 grados)
-pwm.start(7.5) # Puede necesitar ajustar este valor según tu servo
+pwm.start(8) # Puede necesitar ajustar este valor según tu servo
 
 # Espera un tiempo para que el servo se mueva a la posición inicial
-time.sleep(1)
+time.sleep(10)
 
 # Detén el servo (opcional)
 pwm.stop()
