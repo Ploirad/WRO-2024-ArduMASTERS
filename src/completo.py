be338e0c39e5e4d2ace0e200d35574a664198ce9
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-03-03 21:22:55 +0100

    Update completo.py

--- src/completo.py
+++ src/completo.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
-# Define los pines Trig y Echo para cada sensor ultrasónico
+# Define los pines
 TRIG_PIN_DELANTE = 23
 ECHO_PIN_DELANTE = 24
 TRIG_PIN_ATRAS = 17
@@ -10,24 +10,36 @@ TRIG_PIN_IZQUIERDA = 22
 ECHO_PIN_IZQUIERDA = 10
 TRIG_PIN_DERECHA = 5
 ECHO_PIN_DERECHA = 6
+servo_pin_direccion = 18
+servo_pin_traccion = 17
 
-# Define variables para almacenar las distancias medidas por cada sensor
+# Define variables
+empezado = 0
 distancia_delante = 0
 distancia_atras = 0
 distancia_izquierda = 0
 distancia_derecha = 0
+distancia_comienzo_derecha = 0
+distancia_comienzo_izquierda = 0
+valor_d = 7.5 #Direccion 2.5=izq; 7.5=centro; 12.5=der
+valor_t = 7   #Traccion 6=Atras;  8=Alante;  7=stop
 
-def setup():
-    # Configura los pines GPIO
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(TRIG_PIN_DELANTE, GPIO.OUT)
-    GPIO.setup(ECHO_PIN_DELANTE, GPIO.IN)
-    GPIO.setup(TRIG_PIN_ATRAS, GPIO.OUT)
-    GPIO.setup(ECHO_PIN_ATRAS, GPIO.IN)
-    GPIO.setup(TRIG_PIN_IZQUIERDA, GPIO.OUT)
-    GPIO.setup(ECHO_PIN_IZQUIERDA, GPIO.IN)
-    GPIO.setup(TRIG_PIN_DERECHA, GPIO.OUT)
-    GPIO.setup(ECHO_PIN_DERECHA, GPIO.IN)
+# Configura los pines GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin_direccion, GPIO.OUT)
+GPIO.setup(servo_pin_traccion, GPIO.OUT)
+GPIO.setup(TRIG_PIN_DELANTE, GPIO.OUT)
+GPIO.setup(ECHO_PIN_DELANTE, GPIO.IN)
+GPIO.setup(TRIG_PIN_ATRAS, GPIO.OUT)
+GPIO.setup(ECHO_PIN_ATRAS, GPIO.IN)
+GPIO.setup(TRIG_PIN_IZQUIERDA, GPIO.OUT)
+GPIO.setup(ECHO_PIN_IZQUIERDA, GPIO.IN)
+GPIO.setup(TRIG_PIN_DERECHA, GPIO.OUT)
+GPIO.setup(ECHO_PIN_DERECHA, GPIO.IN)
+
+#Iniciar servos
+pwm_d = GPIO.PWM(servo_pin_direccion, 50) # Frecuencia de PWM: 50Hz (estándar para servos)
+pwm_t = GPIO.PWM(servo_pin_traccion, 50) # Frecuencia de PWM: 50Hz (estándar para servos)
 
 def get_distance(trig_pin, echo_pin):
     # Envía un pulso al pin Trig
@@ -64,22 +76,36 @@ def cleanup():
     # Limpia los pines GPIO
     GPIO.cleanup()
 
+def primeras_medidas_paredes():
+    global distancia_comienzo_izquierda, distancia_comienzo_derecha
+    distancia_comienzo_izquierda = get_distance(TRIG_PIN_IZQUIERDA, ECHO_PIN_IZQUIERDA)
+    distancia_comienzo_derecha = get_distance(TRIG_PIN_DERECHA, ECHO_PIN_DERECHA)
+
 if __name__ == '__main__':
     try:
         # Configura los pines GPIO
         setup()
 
         while True:
-            # Actualiza las distancias
-            update_distances()
+            if empezado == 0:
+                primeras_medidas_paredes()
+                empezado = 1
+            else:
+                valor_t = 12.5
+                pwm_t.start(valor_t)
+                
+                # Actualiza las distancias
+                update_distances()
 
-            # Muestra las distancias
-            print(f"Distancia hacia delante: {distancia_delante} cm")
-            print(f"Distancia hacia atras: {distancia_atras} cm")
-            print(f"Distancia hacia izquierda: {distancia_izquierda} cm")
-            print(f"Distancia hacia derecha: {distancia_derecha} cm")
-
-            time.sleep(1)
+                #if distancia_delante 
+                
+                # Muestra las distancias
+                print(f"Distancia hacia delante: {distancia_delante} cm")
+                print(f"Distancia hacia atras: {distancia_atras} cm")
+                print(f"Distancia hacia izquierda: {distancia_izquierda} cm")
+                print(f"Distancia hacia derecha: {distancia_derecha} cm")
+        
+                time.sleep(1)
 
     except KeyboardInterrupt:
         # Maneja la interrupción del teclado
