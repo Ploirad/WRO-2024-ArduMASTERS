8652782c3bb6d446e758d86dfb433d0fcf2d2daf
SRMariolo <148376278+SRMariolo@users.noreply.github.com>
2024-03-06 19:06:41 +0100

    Update camara.py

--- src/camara.py
+++ src/camara.py
@@ -3,7 +3,7 @@ import picamera
 def record_video():
     with picamera.PiCamera() as camera:
         camera.resolution = (640, 480)
-        camera.framerate = 30
+        camera.framerate = 20
         camera.start_preview()
         # Allow camera warm-up time
         camera.wait_recording(2)
