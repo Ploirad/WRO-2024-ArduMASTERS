8741c10ecede2290d892bf2efe1c813e2726bf68
Ploirad <dlopezp36@gmail.com>
2024-03-06 19:04:25 +0000

    trying to sove an error

--- src/camara.py
+++ src/camara.py
@@ -1,17 +1,13 @@
 import picamera
 
-def record_video():
-    with picamera.PiCamera() as camera:
-        camera.resolution = (1024, 768)
-        camera.framerate = 30
-        camera.start_preview()
-        # Allow camera warm-up time
-        camera.wait_recording(2)
+with picamera.PiCamera() as camera:
+	camera.resolution = (1024, 768)
+	camera.framerate = 30
+	camera.start_preview()
+	# Allow camera warm-up time
+	camera.wait_recording(2)
 
-        # Record for 30 seconds
-        camera.start_recording('video.h264')
-        camera.wait_recording(30)
-        camera.stop_recording()
-
-if __name__ == '__main__':
-    record_video()
+	# Record for 30 seconds
+	camera.start_recording('video.h264')
+	camera.wait_recording(30)
+	camera.stop_recording()
