19ae1dd3567c01816c1a0a30ac469021d8b89e76
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-03-11 21:55:02 +0100

    Update completo.py

--- src/completo.py
+++ src/completo.py
@@ -84,6 +84,13 @@ def update_distances():
     distancia_izquierda = get_distance(TRIG_PIN_IZQUIERDA, ECHO_PIN_IZQUIERDA)
     distancia_derecha = get_distance(TRIG_PIN_DERECHA, ECHO_PIN_DERECHA)
 
+def giro():
+    pwm_t.start(valor_t)
+    pwm_d.start(valor_d)
+    time.sleep(2)
+    pwm_t.start(valor_t)
+    pwm_d.start(GCent)
+
 while True:
     try:
         pwm_t.start(valor_t)
