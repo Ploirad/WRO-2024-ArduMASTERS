ba327d7b10f2e0f0a1bb87f12530c5797949d942
Darío <148375115+Ploirad@users.noreply.github.com>
2024-03-13 19:39:58 +0100

    Update camara.py

--- src/camara.py
+++ src/camara.py
@@ -1,19 +1,19 @@
 from picamera2 import Picamera2, Preview
 
 def record_video():
-    with picamera2.Picamera2() as camera:
+    with Picamera2.Picamera2() as camera:
         camera.resolution = (640, 480)
         camera.framerate = 30
 
         # Preview configuration
-        preview_config = picamera2.Preview.create()
-        preview_config.set_parameter(picamera2.Preview.Parameter.FRAME_RATE, camera.framerate)
+        preview_config = Picamera2.Preview.create()
+        preview_config.set_parameter(Picamera2.Preview.Parameter.FRAME_RATE, camera.framerate)
         preview_config.set_parameter(picamera2.Preview.Parameter.RESOLUTION, camera.resolution)
 
         # Video configuration
-        video_config = picamera2.Video.create()
-        video_config.set_parameter(picamera2.Video.Parameter.FRAME_RATE, camera.framerate)
-        video_config.set_parameter(picamera2.Video.Parameter.RESOLUTION, camera.resolution)
+        video_config = Picamera2.Video.create()
+        video_config.set_parameter(Picamera2.Video.Parameter.FRAME_RATE, camera.framerate)
+        video_config.set_parameter(Picamera2.Video.Parameter.RESOLUTION, camera.resolution)
 
         camera.start_preview(preview_config)
 
@@ -21,7 +21,7 @@ def record_video():
         camera.wait(2000)
 
         # Create a software encoder
-        encoder = picamera2.SoftwareEncoder(camera, 'mp4', video_config)
+        encoder = Picamera2.SoftwareEncoder(camera, 'mp4', video_config)
 
         # Create a file object for the output video
         with open('video.h264', 'wb') as output_file:
