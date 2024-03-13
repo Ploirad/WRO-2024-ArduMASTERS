5df34b81015408281c66596c2c219e3ae6df703e
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-03-13 19:53:22 +0100

    Update servo_lib.py

--- src/librerias/servo_lib.py
+++ src/librerias/servo_lib.py
@@ -1,1 +1,26 @@
+import RPI.GPIO as GPIO
 
+class servos:
+  def __init__(self, traccion_pin, direccion_pin):
+    self.servo_pin_direccion = direccion_pin
+    self.servo_pin_traccion = traccion_pin
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(self.servo_pin_direccion, GPIO.OUT)
+    GPIO.setup(self.servo_pin_traccion, GPIO.OUT)
+    self.pwm_d = GPIO.PWM(self.servo_pin_direccion, 50) # Frecuencia de PWM: 50Hz (estándar para servos)
+    self.pwm_t = GPIO.PWM(self.servo_pin_traccion, 50) # Frecuencia de PWM: 50Hz (estándar para servos)
+  
+  def forward():
+    pwm_t.start(12.5)
+  
+  def backward():
+    pwm_t.start(2.5)
+  
+  def turn_left():
+    pwm_d.start(11.5)
+  
+  def turn_right():
+    pwm_d.start(3.5)
+  
+  def turn_center():
+    pwm_d.start(6.0)
