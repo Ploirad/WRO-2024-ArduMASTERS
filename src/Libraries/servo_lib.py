c61a1ebdc46bb1e60258d4ba93983328603b6eaf
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-03-14 18:42:25 +0100

    Update servo_lib.py

--- src/Libraries/servo_lib.py
+++ src/Libraries/servo_lib.py
@@ -10,17 +10,17 @@ class servos:
     self.pwm_d = GPIO.PWM(self.servo_pin_direccion, 50) # Frecuencia de PWM: 50Hz (estándar para servos)
     self.pwm_t = GPIO.PWM(self.servo_pin_traccion, 50) # Frecuencia de PWM: 50Hz (estándar para servos)
   
-  def forward():
-    pwm_t.start(12.5)
+  def forward(self):
+    self.pwm_t.start(12.5)
   
-  def backward():
-    pwm_t.start(2.5)
+  def backward(self):
+    self.pwm_t.start(2.5)
   
-  def turn_left():
-    pwm_d.start(11.5)
+  def turn_left(self):
+    self.pwm_d.start(11.5)
   
-  def turn_right():
-    pwm_d.start(3.5)
+  def turn_right(self):
+    self.pwm_d.start(3.5)
   
-  def turn_center():
-    pwm_d.start(6.0)
+  def turn_center(self):
+    self.pwm_d.start(6.0)
