7c28298d639439143f9a379c497c43e5e202a9f3
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-03-14 19:47:07 +0100

    Update main_code.py

--- src/Libraries/main_code.py
+++ src/Libraries/main_code.py
@@ -17,8 +17,8 @@ button_pin = 9
 estado_boton = False
 empezar = False
 
-ultrasonidos = Us.Ultrasound(TRIG_pin_DELANTE, ECHO_pin_DELANTE, TRIG_pin_ATRAS, ECHO_pin_ATRAS, TRIG_pin_IZQUIERDA, ECHO_pin_IZQUIERDA, TRIG_pin_DERECHA, ECHO_pin_DERECHA)
-servos = Servomotor.servos(servo_pin_traccion, servo_pin_direccion)
+#ultrasonidos = Us.Ultrasound(TRIG_pin_DELANTE, ECHO_pin_DELANTE, TRIG_pin_ATRAS, ECHO_pin_ATRAS, TRIG_pin_IZQUIERDA, ECHO_pin_IZQUIERDA, TRIG_pin_DERECHA, ECHO_pin_DERECHA)
+#servos = Servomotor.servos(servo_pin_traccion, servo_pin_direccion)
 Boton = boton.Button(button_pin)
 while True:
   estado_boton = Boton.button_lecture()
