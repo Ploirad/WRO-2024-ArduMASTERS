5752f78874165d8a5700ece72e34286a99e5cd8e
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-03-14 18:40:54 +0100

    Update ultrasound_lib.py

--- src/Libraries/ultrasound_lib.py
+++ src/Libraries/ultrasound_lib.py
@@ -1,24 +1,26 @@
 import RPi.GPIO as GPIO
+import time
 
 class Ultrasound:
     def __init__(self, Tforw, Eforw, Tback, Eback, TI, EI, TD, ED):
-      self.Tforw = Tforw
-      self.Eforw = Eforw
-      self.Tback = Tback
-      self.Eback = Eback
-      self.TI = TI
-      self.EI = EI
-      self.TD = TD
-      self.ED = ED
-      GPIO.setmode(GPIO.BCM)
-      GPIO.setup(Tforw, GPIO.OUT)
-      GPIO.setup(Eforw, GPIO.IN)
-      GPIO.setup(Tback, GPIO.OUT)
-      GPIO.setup(Eback, GPIO.IN)
-      GPIO.setup(TI, GPIO.OUT)
-      GPIO.setup(EI, GPIO.IN)
-      GPIO.setup(TD, GPIO.OUT)
-      GPIO.setup(ED, GPIO.IN)
+        self.Tforw = Tforw
+        self.Eforw = Eforw
+        self.Tback = Tback
+        self.Eback = Eback
+        self.TI = TI
+        self.EI = EI
+        self.TD = TD
+        self.ED = ED
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(Tforw, GPIO.OUT)
+        GPIO.setup(Eforw, GPIO.IN)
+        GPIO.setup(Tback, GPIO.OUT)
+        GPIO.setup(Eback, GPIO.IN)
+        GPIO.setup(TI, GPIO.OUT)
+        GPIO.setup(EI, GPIO.IN)
+        GPIO.setup(TD, GPIO.OUT)
+        GPIO.setup(ED, GPIO.IN)
+        
 
     def get_distance(trig_pin, echo_pin):
         # Envía un pulso al pin Trig
@@ -36,15 +38,15 @@ class Ultrasound:
         distance = round(distance, 3)
         return distance
 
-    def get_US_forward():
-        df = get_distance(Tforw, Eforw)
+    def get_US_forward(self):
+        df = self.get_distance(Tforw, Eforw)
         return df
     def get_US_backward():
-        db = get_distance(Tback, Eback)
+        db = self.get_distance(Tback, Eback)
         return db
     def get_US_left():
-        dl = get_distance(TI, EI)
+        dl = self.get_distance(TI, EI)
         return dl
     def get_US_right():
-        dr = get_distance(TD, ED)
+        dr = self.get_distance(TD, ED)
         return dr
