a895ac2df0e48ec5e716fdbc6fc0de60c155a603
Ploirad <dlopezp36@gmail.com>
2024-03-20 19:52:42 +0100

    update camera.py

--- src/camera.py
+++ src/camera.py
@@ -1,20 +1,9 @@
 import time
-from picamera2 import Picamera2, Preview
-from picamera2.encoders import H264Encoder
+import picamera2
 
-def record_video(): 
-    with Picamera2() as picam2: 
-        picam2.resolution = (640, 480) 
-        picam2.framerate = 3 
-        preview = picam2.create_video_preview() 
-        picam2.start_preview(preview) 
-        encoder = H264Encoder() 
-        picam2.start_recording(encoder, 'output.h264') 
-        time.sleep(5) 
-        picam2.stop_recording() 
-        picam2.stop_preview()
-if  __name__ == "__main__":
-    try:
-        record_video()
-    except KeyboardInterrupt:
-        print("Stopping recording...")
+with picamera2. Picamera2() as camera:
+    camera.configure(picamera2.PreviewDefinition(width=640, height=480))
+    camera.start()
+    time.sleep(5) 
+    camera.stop_recording() 
+    camera.stop_preview()
