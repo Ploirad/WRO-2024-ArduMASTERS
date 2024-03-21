96abf0f7330ab4e8d933a99e0d7867b4556d5acd
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-03-21 21:57:31 +0100

    Update complete.py

--- src/complete.py
+++ src/complete.py
@@ -228,8 +228,5 @@ while True:
                 print(f"Vueltas:{float(vueltas/8)} es decir {vueltas} giros")
             except:
                 print("ERROR")
-                if KeyboardInterrupt:
-                    v = 0
-                    GPIO.cleanup()
     except KeyboardInterrupt:
         GPIO.cleanup()
