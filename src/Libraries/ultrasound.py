e24e00190a31e28370d1600ef26985eb0212bcf7
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-03-22 21:15:45 +0100

    Update ultrasound.py

--- src/Libraries/ultrasound.py
+++ src/Libraries/ultrasound.py
@@ -25,18 +25,13 @@ def get_distance(trig_pin, echo_pin):
     GPIO.output(trig_pin, True)
     time.sleep(0.00001)
     GPIO.output(trig_pin, False)
-    
     pulse_start = time.time()
     pulse_end = time.time()
-    
     while GPIO.input(echo_pin) == 0:
         pulse_start = time.time()
-    
     while GPIO.input(echo_pin) == 1:
         pulse_end = time.time()
-    
     pulse_duration = pulse_end - pulse_start
     distance = pulse_duration * 17150
     distance = round(distance, 2)
-    
     return distance
