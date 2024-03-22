526839d4c5650a0092bb18d6a9eb569cdebccb2e
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-03-22 19:46:42 +0100

    Update main.py

--- src/Libraries/main.py
+++ src/Libraries/main.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 from servo_control import *
+from ultrasound import *
 
 button_pin = 9
 
@@ -11,8 +12,9 @@ def setup_GPIO():
 def main():
     setup_GPIO()  # Configurar pines GPIO
     
-    # Inicializar servos
+    # Inicializar servos y ultrasonidos
     init_servos()
+    init_ultrasonido()
     
     v = 0
     while True:
@@ -34,6 +36,18 @@ def main():
                 time.sleep(2)    # Espera 2 segundos
                 giro(12.5, 3.5)  # Ejemplo: mover hacia adelante y girar a la izquierda
                 time.sleep(2)    # Espera 2 segundos
+                
+                # Ejemplo de uso de los ultrasonidos
+                distancia_delante = get_distance(Tforw, Eforw)
+                distancia_atras = get_distance(Tback, Eback)
+                distancia_izquierda = get_distance(TI, EI)
+                distancia_derecha = get_distance(TD, ED)
+                
+                print("Distancia delante:", distancia_delante, "cm")
+                print("Distancia atrás:", distancia_atras, "cm")
+                print("Distancia izquierda:", distancia_izquierda, "cm")
+                print("Distancia derecha:", distancia_derecha, "cm")
+                time.sleep(1)  # Espera 1 segundo
             
         except KeyboardInterrupt:
             GPIO.cleanup()
