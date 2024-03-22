f5e1ea3405a88fb07d48d01b9a824f25755bbc86
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-03-22 19:35:01 +0100

    Update ultrasound.py

--- src/Libraries/ultrasound.py
+++ src/Libraries/ultrasound.py
@@ -12,14 +12,14 @@ ED = 6
 
 def init_ultrasonido():
     GPIO.setmode(GPIO.BCM)
-        GPIO.setup(Tforw, GPIO.OUT)
-        GPIO.setup(Eforw, GPIO.IN)
-        GPIO.setup(Tback, GPIO.OUT)
-        GPIO.setup(Eback, GPIO.IN)
-        GPIO.setup(TI, GPIO.OUT)
-        GPIO.setup(EI, GPIO.IN)
-        GPIO.setup(TD, GPIO.OUT)
-        GPIO.setup(ED, GPIO.IN)
+    GPIO.setup(Tforw, GPIO.OUT)
+    GPIO.setup(Eforw, GPIO.IN)
+    GPIO.setup(Tback, GPIO.OUT)
+    GPIO.setup(Eback, GPIO.IN)
+    GPIO.setup(TI, GPIO.OUT)
+    GPIO.setup(EI, GPIO.IN)
+    GPIO.setup(TD, GPIO.OUT)
+    GPIO.setup(ED, GPIO.IN)
 
 def get_distance(trig_pin, echo_pin):
     GPIO.output(trig_pin, True)
