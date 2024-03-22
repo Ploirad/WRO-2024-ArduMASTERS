20c599b3e63205547331a75d08031801e9a24cd8
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-03-22 21:09:21 +0100

    Update servo_control.py

--- src/Libraries/servo_control.py
+++ src/Libraries/servo_control.py
@@ -14,25 +14,19 @@ def giro(valor_t, valor_d):
     
     pwm_d.start(valor_d)
     pwm_t.start(valor_t)
-    # Resto del código de giro
-
+    
     time.sleep(2)
-    pwm_d.start(6.0)
-    pwm_t.start(valor_t)
+    pwm_d.ChangeDutyCycle(6.0)  # Modificar el ciclo de trabajo en lugar de reiniciar
+    pwm_t.ChangeDutyCycle(valor_t)  # Modificar el ciclo de trabajo en lugar de reiniciar
     time.sleep(2)
-    pwm_d.start(6.0)
-    pwm_t.start(valor_t)
+    pwm_d.ChangeDutyCycle(6.0)  # Modificar el ciclo de trabajo en lugar de reiniciar
+    pwm_t.ChangeDutyCycle(valor_t)  # Modificar el ciclo de trabajo en lugar de reiniciar
     if valor_d == 11.5:
-        pwm_d.start(3.5)
+        pwm_d.ChangeDutyCycle(3.5)  # Modificar el ciclo de trabajo en lugar de reiniciar
     elif valor_d == 3.5:
-        pwm_d.start(11.5)
+        pwm_d.ChangeDutyCycle(11.5)  # Modificar el ciclo de trabajo en lugar de reiniciar
     else:
-        pwm_d.start(6.0)
+        pwm_d.ChangeDutyCycle(6.0)  # Modificar el ciclo de trabajo en lugar de reiniciar
     time.sleep(2)
     valor_t = 12.5
     valor_d = 6.0
-def avance_recto():
-    pwm_d = GPIO.PWM(servo_pin_direccion, 50)
-    pwm_t = GPIO.PWM(servo_pin_traccion, 50)
-    pwm_d.start(6.0)
-    pwm_t.start(12.5)
