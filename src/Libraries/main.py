6d9fc725cec17cac9b76d046555973c12d30af3d
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-03-22 19:51:44 +0100

    Update main.py

--- src/Libraries/main.py
+++ src/Libraries/main.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import time
 from servo_control import *
 from ultrasound import *
+from siguelineas import *
 
 button_pin = 9
 
@@ -11,10 +12,9 @@ def setup_GPIO():
 
 def main():
     setup_GPIO()  # Configurar pines GPIO
-    
-    # Inicializar servos y ultrasonidos
     init_servos()
     init_ultrasonido()
+    init_siguelineas()
     
     v = 0
     while True:
@@ -32,7 +32,11 @@ def main():
                 distancia_atras = get_distance(Tback, Eback)
                 distancia_izquierda = get_distance(TI, EI)
                 distancia_derecha = get_distance(TD, ED)
-                
+
+                if linea_detectada():
+                    print("Línea detectada")
+                else:
+                    print("No se detecta línea")
                 print("Distancia delante:", distancia_delante, "cm")
                 print("Distancia atrás:", distancia_atras, "cm")
                 print("Distancia izquierda:", distancia_izquierda, "cm")
