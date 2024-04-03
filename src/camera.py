580a1a1d8b5417ff78997b88948717552120a7d4
Darío <148375115+Ploirad@users.noreply.github.com>
2024-04-03 18:44:27 +0200

    Update camera.py

--- src/camera.py
+++ src/camera.py
@@ -2,4 +2,4 @@ import time
 import picamera2
 
 with picamera2. Picamera2() as camera:
-picamera2.start_and_record_video("test.mp4", duration=10)
+  picamera2.start_and_record_video("test.mp4", duration=10)
