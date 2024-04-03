54f306fdcacb6736e500f8317957a27768877700
Ploirad <dlopezp36@gmail.com>
2024-04-03 19:56:48 +0200

    updated camera.py

--- src/camera.py
+++ src/camera.py
@@ -10,6 +10,6 @@ picam2.configure(video_config)
 
 encoder = H264Encoder(10000000)
 
-picam2.start_recording(mjpeg,'test.mp')
+picam2.start_recording(encoder,'test.mp')
 time.sleep(10)
 picam2.stop_recording()
