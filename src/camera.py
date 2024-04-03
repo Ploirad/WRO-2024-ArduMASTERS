0445509e94b754177788d28bbd0f1efcbea32d2b
Ploirad <dlopezp36@gmail.com>
2024-04-03 18:28:23 +0200

    updated camera.py

--- src/camera.py
+++ src/camera.py
@@ -2,7 +2,6 @@ import time
 import picamera2
 
 with picamera2. Picamera2() as camera:
-    camera.configure(picamera2.start_preview()(width=640, height=480))
     camera.start()
     time.sleep(5) 
     camera.stop_recording() 
