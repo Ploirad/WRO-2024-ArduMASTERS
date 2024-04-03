cfcea23bdf78f110938da6bf3a16a59a891a74fd
Ploirad <dlopezp36@gmail.com>
2024-04-03 19:16:15 +0200

    updated camera.py

--- src/camera.py
+++ src/camera.py
@@ -1,30 +1,15 @@
 #!/usr/bin/python3
-
-import socket
 import time
 
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
-from picamera2.outputs import FileOutput
 
 picam2 = Picamera2()
-video_config = picam2.create_video_configuration({"size": (1280, 720)})
+video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
-encoder = H264Encoder(1000000)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(("0.0.0.0", 10001))
-    sock.listen()
+encoder = H264Encoder(10000000)
 
-    picam2.encoders = encoder
-
-    conn, addr = sock.accept()
-    stream = conn.makefile("wb")
-    encoder.output = FileOutput(stream)
-    picam2.start_encoder(encoder)
-    picam2.start()
-    time.sleep(20)
-    picam2.stop()
-    picam2.stop_encoder()
-    conn.close()
+picam2.start_recording(encoder, 'test.h264')
+time.sleep(10)
+picam2.stop_recording()
