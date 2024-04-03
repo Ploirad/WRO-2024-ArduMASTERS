3d7c49538cc8c44fd54206623a3ba62b06408491
Ploirad <dlopezp36@gmail.com>
2024-04-03 18:57:39 +0200

    updated camera.py

--- src/camera.py
+++ src/camera.py
@@ -2,14 +2,14 @@
 import time
 
 from picamera2 import Picamera2
-from picamera2.outputs import FfmpegOutput
+from picamera2.encoders import H264Encoder
 
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
 
-output = FfmpegOutput('test.mp4', audio=True)
+encoder = H264Encoder(10000000)
 
-picam2.start_recording(output)
+picam2.start_recording(encoder, 'test.h264')
 time.sleep(10)
 picam2.stop_recording()
