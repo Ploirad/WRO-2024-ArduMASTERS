70ef52dba3304b32badc4931d22d6a23377a97d1
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-04-07 14:05:40 +0200

    Update complete.py

--- src/complete.py
+++ src/complete.py
@@ -27,7 +27,7 @@ distancia_comienzo_derecha = 0
 distancia_comienzo_izquierda = 0
 valor_d = 7.5
 valor_t = 7.0   #Traccion 2.5=Atras;  12.5=Alante;  7=stop
-DISTANCIA_de_ACCION = {"MENOR QUE": 15, "MAYOR QUE": 14}
+DISTANCIA_de_ACCION = {"MENOR QUE": 20, "MAYOR QUE": 19}
 TAvance = 12.5
 TAtras = 2.5
 GDer = 3.5
