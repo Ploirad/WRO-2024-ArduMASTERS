c29ccfe85de77f56a3314cd9637a7fc9027a1f81
Darío <148375115+Ploirad@users.noreply.github.com>
2024-04-10 19:23:10 +0200

    git pull

--- src/camera.py
+++ src/camera.py
@@ -2,13 +2,13 @@
 import time
 
 from picamera2 import Picamera2
-from picamera2.encoders import H264Encoder
+from picamera2.encoders import Encoder
 
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
 
-encoder = H264Encoder()
+encoder = Encoder()
 
 picam2.start_recording(encoder,'test.h264')
 time.sleep(10)
