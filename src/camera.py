059cdac77aaeaf3b70b79f42146a8ff35a5c39da
Darío <148375115+Ploirad@users.noreply.github.com>
2024-04-10 18:53:50 +0200

    git push

--- src/camera.py
+++ src/camera.py
@@ -10,6 +10,6 @@ picam2.configure(video_config)
 
 encoder = H264Encoder()
 
-picam2.start_recording(encoder,'test.mp')
+picam2.start_recording(encoder,'test.h264')
 time.sleep(10)
 picam2.stop_recording()
