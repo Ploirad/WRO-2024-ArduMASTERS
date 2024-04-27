c2394f46363ef78564f1a22629151eb8567bd992
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-04-27 17:54:31 +0200

    Update complete.py

--- src/complete.py
+++ src/complete.py
@@ -221,7 +221,7 @@ while True:
             print(f"NumberLinea:{numberlinea}")
             print(f"Linea:{linea}")
             print(f"Vueltas:{float(vueltas/x)} es decir {vueltas} giros")
-    except e:
+    except e = exception:
         if e == KeyboardInterrupt:
             GPIO.cleanup()
         else:
