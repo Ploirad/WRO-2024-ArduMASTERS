0aa73eb3cb111c6b0590d0dde0a203efafe774a9
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-05-18 21:34:56 +0200

    Update complete_prueba2.py

--- src/complete_prueba2.py
+++ src/complete_prueba2.py
@@ -129,14 +129,14 @@ def detect_colors(frame):
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     # 238, 39, 55 ///  68, 214, 44 /// 255, 0, 255
     # Nuevos rangos de color
-    lower_red = np.array([170, 231, 56])
-    upper_red = np.array([178, 255, 69])
+    lower_red = np.array([2, 86, 50])
+    upper_red = np.array([2, 86, 100])
     
-    lower_green = np.array([43, 160, 29])
-    upper_green = np.array([66, 255, 55])
+    lower_green = np.array([127, 60, 50])
+    upper_green = np.array([127, 60, 90])
     
-    lower_magenta = np.array([145, 174, 51])
-    upper_magenta = np.array([160, 218, 65])
+    lower_magenta = np.array([332, 76, 56])
+    upper_magenta = np.array([332, 76, 100])
 
     # Detección de colores
     mask_red = cv2.inRange(hsv_frame, lower_red, upper_red)
