110cd6ed9dd0ca56100b7d2bcbbeb01db88ffa2b
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-05-29 18:58:17 +0200

    Update servos.py

--- src/servos.py
+++ src/servos.py
@@ -27,8 +27,6 @@ pwm_t = GPIO.PWM(servo_pin_traccion, 50) # Frecuencia de PWM: 50Hz (estándar pa
 
 while True:
     button_state = GPIO.input(button_pin)
-    pwm_d.start(valor_d)
-    pwm_t.start(valor_t)
     if button_state == GPIO.HIGH:
         print("Botón presionado")
         arrancar = True
@@ -41,6 +39,8 @@ while True:
       if arrancar:
         valor_d = GCent
         valor_t = TAvance
+        pwm_d.start(valor_d)
+        pwm_t.start(valor_t)
     except KeyboardInterrupt:
         GPIO.cleanup()
     except:
