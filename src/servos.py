3b4bbd9bda491a125db298d9976c4f81578e854c
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-05-29 18:47:43 +0200

    Update servos.py

--- src/servos.py
+++ src/servos.py
@@ -1,1 +1,44 @@
+import RPi.GPIO as GPIO
 
+# Define los pines
+servo_pin_direccion = 2
+servo_pin_traccion = 3
+button_pin = 9
+
+# Define variables
+TAvance = 12.5
+TAtras = 2.5
+GDer = 4.5
+GIzq = 10.5
+GCent = 6#5.9
+valor_d = GCent
+valor_t = TAvance
+
+# Configura los pines GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin_direccion, GPIO.OUT)
+GPIO.setup(servo_pin_traccion, GPIO.OUT)
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+#Iniciar servos
+pwm_d = GPIO.PWM(servo_pin_direccion, 50) # Frecuencia de PWM: 50Hz (estándar para servos)
+pwm_t = GPIO.PWM(servo_pin_traccion, 50) # Frecuencia de PWM: 50Hz (estándar para servos)
+
+while True:
+    button_state = GPIO.input(button_pin)
+    if button_state == GPIO.HIGH:
+        print("Botón presionado")
+        arrancar = True
+
+    if button_state == GPIO.LOW and arrancar:
+        v = 1
+        arrancar = False
+
+    try:
+      if arrancar:
+        valor_d = GCent
+        valor_t = TAvance
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+    except:
+        pass
