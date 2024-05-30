d0898064e32d12e5b5fdfc7d017145bcb2f09d5b
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-05-30 21:08:13 +0200

    Update Motor.py

--- src/Main/Libraries/Motor.py
+++ src/Main/Libraries/Motor.py
@@ -3,8 +3,8 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.OUT)
 GPIO.setup(3, GPIO.OUT)
 
-Motor = GPIO.PWM(2,50)
-Direccion = GPIO.PWM(3,50)
+Motor = GPIO.PWM(3, 50)
+Direccion = GPIO.PWM(2, 50)
 
 def movimiento(vel,dir,stop):
    
@@ -20,12 +20,7 @@ def movimiento(vel,dir,stop):
         Direccion.start(4.5+dir/30)
     else:
         Motor.stop()
-        Direccion.start(6)
-
-import time
+        Direccion.start(7.5)
 
 while True:
-   movimiento(100, 180, 0)
-   time.sleep(5)
    movimiento(100, 0, 0)
-   time.sleep(5)
