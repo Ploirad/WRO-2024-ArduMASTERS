97fa5be4ba1912743ffea1e51cbb8c897bdb0cf4
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-05-31 19:30:39 +0200

    Update prueba.py

--- src/Main/Libraries/prueba.py
+++ src/Main/Libraries/prueba.py
@@ -11,21 +11,20 @@ arrancado = False
 while True:
   if arrancado:
     print("vuelta")
-    t1 = time.time()
     DDelantera = HC.measure_distance(1)
-    print (time.time() - t1)
     DDerecha = HC.measure_distance(2)
-    print (time.time() - t1)
     DIzquierda = HC.measure_distance(4)
-    print (time.time() - t1)
-    if DDelantera > 20:
+    print(DDelantera)
+    print(DDerecha)
+    print(DIzquierda)
+    if DDelantera > 30:
       if DDerecha < 6 and DIzquierda > 6:
         M.movimiento(1, -1, TF)
       elif DIzquierda < 6 and DDerecha > 6:
         M.movimiento(1, 1, TF)
       else:
         M.movimiento(1, 0, TF)
-    elif DDelantera > 6:
+    elif DDelantera > 5:
       M.movimiento(1, (-1 if DDerecha > DIzquierda else (1 if DIzquierda > DDerecha else DDerecha)), TF)
     else:
       M.movimiento(-1, (1 if DDerecha > DIzquierda else (-1 if DIzquierda > DDerecha else DDerecha)), TF)
