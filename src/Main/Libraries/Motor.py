999133d8b92a1941b1b431755ec942ea3a65c9ed
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-05-31 13:36:23 +0200

    Update Motor.py

--- src/Main/Libraries/Motor.py
+++ src/Main/Libraries/Motor.py
@@ -7,30 +7,28 @@ Motor = GPIO.PWM(3, 50)
 Direccion = GPIO.PWM(2, 50)
 
 def movimiento(vel, dir, stop):
+    stop_direccion = False
     if vel > 0:
         sen = 12.5
     elif vel < 0: 
         sen = 2.5
     else:
-        stop = 1
+        stop_direccion = True
 
     if not stop:
         Motor.start(sen)
         #Direccion.start(4.5+(dir/30))
-        if dir > 0:
-            Direccion.start(2.5)
-        elif dir < 0:
-            Direccion.start(12.5)
+        if not stop_direccion:
+            if dir > 0:
+                Direccion.start(2.5)
+            elif dir < 0:
+                Direccion.start(12.5)
+            else:
+                Direccion.start(10.5)
         else:
             Direccion.start(10.5)
     else:
         Motor.stop()
-        Direccion.start(7.5)
+        Direccion.start(10.5)
 
-import time
-a = int(input("a: "))
-b = int(input("b: "))
-while True:
-    t1 = time.time()
-    movimiento(a, b, 0)
-    print(time.time()-t1)
+direccion(0, 0, True)
