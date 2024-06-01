0e816a605b2bea5252d0164428db4c45efd815f5
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-06-01 12:37:01 +0200

    Update prueba.py

--- src/Main/Libraries/prueba.py
+++ src/Main/Libraries/prueba.py
@@ -13,33 +13,26 @@ DDelantera = 0.0
 while True:
   if arrancado:
     print("vuelta")
-    DDelanteraAnteriorTruncada = (int((100*DDelantera)))/100
     DDelantera = HC.measure_distance(1)
     DDerecha = HC.measure_distance(2)
     DIzquierda = HC.measure_distance(4)
     print(DDelantera)
     print(DDerecha)
-    print(DIzquierda)
-
-    if not (((int((100*DDelantera)))/100) == DDelanteraAnteriorTruncada):
-      if DDelantera > 30:
-        if DDerecha < 6 and DIzquierda > 6:
-          M.movimiento(1, -1, TF)
-        elif DIzquierda < 6 and DDerecha > 6:
-          M.movimiento(1, 1, TF)
-        else:
-          M.movimiento(1, 0, TF)
-      elif DDelantera > 5:
-        M.movimiento(1, (1 if DDerecha > DIzquierda else (-1 if DIzquierda > DDerecha else DDerecha)), TF)
+  print(DIzquierda)
+    if DDelantera > 30:
+      if DDerecha < 6 and DIzquierda > 6:
+        M.movimiento(1, -1, TF)
+      elif DIzquierda < 6 and DDerecha > 6:
+        M.movimiento(1, 1, TF)
       else:
-        M.movimiento(-1, (1 if DDerecha > DIzquierda else (-1 if DIzquierda > DDerecha else DDerecha)), TF)
-        DTrasera = HC.measure_distance(3)
-        if DTrasera < 5:
-          M.movimiento(1, 0, TF)
+        M.movimiento(1, 0, TF)
+    elif DDelantera > 5:
+      M.movimiento(1, (1 if DDerecha > DIzquierda else (-1 if DIzquierda > DDerecha else DDerecha)), TF)
     else:
-      print(f"retroceso, {((int((100*DDelantera)))/100)} = {DDelanteraAnteriorTruncada}")
-      M.movimiento(-1, 0, TF)
-      time.sleep(1)
+      M.movimiento(-1, (1 if DDerecha > DIzquierda else (-1 if DIzquierda > DDerecha else DDerecha)), TF)
+      DTrasera = HC.measure_distance(3)
+      if DTrasera < 5:
+        M.movimiento(1, 0, TF)
   else:
     print("esperando al boton")
     if GPIO.input(9) == GPIO.HIGH:
