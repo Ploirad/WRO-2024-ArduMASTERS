ad512501d40978d465c49c62007552ef9be334f6
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-06-04 21:35:29 +0200

    Update p.py

--- src/Main/Libraries/p.py
+++ src/Main/Libraries/p.py
@@ -2,20 +2,27 @@ import cv2
 
 # Abre la cámara
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+# Verifica si la cámara se ha abierto correctamente
+if not cap.isOpened():
+    print("Error al abrir la cámara")
+    exit()
 
 while True:
     # Lee un fotograma de la cámara
     ret, frame = cap.read()
 
-    # Muestra el fotograma en una ventana
-    cv2.imshow('Camera', frame)
+    # Verifica si se ha leído correctamente el fotograma
+    if not ret:
+        print("Error al capturar el fotograma")
+        break
 
-    # Espera a que se presione la tecla 'q' para salir
+    # Guarda el fotograma en el disco
+    cv2.imwrite('captura.jpg', frame)
+
+    # Sal del bucle si se presiona la tecla 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Libera la cámara y cierra todas las ventanas
+# Libera la cámara
 cap.release()
-cv2.destroyAllWindows()
