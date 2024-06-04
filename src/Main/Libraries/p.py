40f9d708a34c1f8793984a5cbfb6a35c1977c6b1
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-06-04 21:58:38 +0200

    Update p.py

--- src/Main/Libraries/p.py
+++ src/Main/Libraries/p.py
@@ -1,7 +1,3 @@
-import time
 import picamera
-
-# Inicializa la cámara
-with picamera.PiCamera() as camera:
-    # Captura una imagen
-    camera.capture('captura.jpg')
+camera = picamera.PiCamera()
+camera.capture('captura.jpg')
