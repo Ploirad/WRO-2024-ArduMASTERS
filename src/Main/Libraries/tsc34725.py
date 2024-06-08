3eaf5a62aca3b50b1d2eedd2ffce0ea56dd28b63
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-06-08 14:33:06 +0200

    Update tsc34725.py

--- src/Main/Libraries/tsc34725.py
+++ src/Main/Libraries/tsc34725.py
@@ -1,44 +1,22 @@
-import pigpio
+import smbus2
 import time
 
 def read_color():
-    # Inicializar el objeto PiGPIO
-    pi = pigpio.pi()
-
-    # Dirección del sensor TCS34725
     TCS34725_ADDRESS = 0x29
-
-    # Registro de control
-    TCS34725_ENABLE = 0x00
-    TCS34725_ATIME = 0x01
-    TCS34725_CONTROL = 0x0F
-    TCS34725_ID = 0x12
     TCS34725_CDATAL = 0x14
     TCS34725_RDATAL = 0x16
     TCS34725_GDATAL = 0x18
     TCS34725_BDATAL = 0x1A
 
-    # Configurar el sensor TCS34725
-    pi.i2c_write_byte_data(TCS34725_ADDRESS, TCS34725_ENABLE, 0x01)
-    pi.i2c_write_byte_data(TCS34725_ADDRESS, TCS34725_ATIME, 0xEB)
-    pi.i2c_write_byte_data(TCS34725_ADDRESS, TCS34725_CONTROL, 0x03)
+    bus = smbus2.SMBus(1)
+    clear = bus.read_word_data(TCS34725_ADDRESS, TCS34725_CDATAL)
+    red = bus.read_word_data(TCS34725_ADDRESS, TCS34725_RDATAL)
+    green = bus.read_word_data(TCS34725_ADDRESS, TCS34725_GDATAL)
+    blue = bus.read_word_data(TCS34725_ADDRESS, TCS34725_BDATAL)
 
-    # Esperar a que el sensor se estabilice
-    time.sleep(0.5)
-
-    # Leer los valores de color
-    red = pi.i2c_read_word_data(TCS34725_ADDRESS, TCS34725_RDATAL)
-    green = pi.i2c_read_word_data(TCS34725_ADDRESS, TCS34725_GDATAL)
-    blue = pi.i2c_read_word_data(TCS34725_ADDRESS, TCS34725_BDATAL)
-    clear = pi.i2c_read_word_data(TCS34725_ADDRESS, TCS34725_CDATAL)
-
-    # Cerrar la conexión con PiGPIO
-    pi.stop()
-
-    # Devolver los valores de color
-    return red, green, blue, clear
+    return clear, red, green, blue
 
 # Ejemplo de uso
 if __name__ == "__main__":
-    r, g, b, c = read_color()
+    c, r, g, b = read_color()
     print(f"Rojo: {r}, Verde: {g}, Azul: {b}, Claro: {c}")
