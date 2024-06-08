75c0ae70fcccbdb3965274f96c9045bb6de9ae25
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-06-08 13:23:26 +0200

    Update camara_nueva.py

--- src/Main/Libraries/camara_nueva.py
+++ src/Main/Libraries/camara_nueva.py
@@ -1,10 +1,7 @@
-# nombre del archivo: color_detection.py
-
 import picamera
 import picamera.array
 import cv2
 import numpy as np
-
 def detectar_color(frame):
     R_bajo = np.array([175, 126, 68])
     R_alto = np.array([176, 212, 255])
@@ -12,25 +9,14 @@ def detectar_color(frame):
     V_alto = np.array([65, 156, 255])
     M_bajo = np.array([138, 87, 25])
     M_alto = np.array([167, 185, 255])
-
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask_r = cv2.inRange(hsv, R_bajo, R_alto)
     mask_v = cv2.inRange(hsv, V_bajo, V_alto)
     mask_m = cv2.inRange(hsv, M_bajo, M_alto)
-    
-    # Mostrar las imágenes binarias de cada color
-    print("Mascara Roja:")
-    print(mask_r)
-    print("Mascara Verde:")
-    print(mask_v)
-    print("Mascara Magenta:")
-    print(mask_m)
-    
     # Calcular el centroide para cada color
     cr = calcular_centroide(mask_r)
     cv = calcular_centroide(mask_v)
     cm = calcular_centroide(mask_m)
-    
     return cr, cv, cm
 
 def calcular_centroide(mask):
@@ -50,20 +36,12 @@ def obtener_centroides(resolution=(640, 480)):
             # Esperar un momento para que la cámara se estabilice
             camera.start_recording('/dev/null', format='h264', motion_output='/dev/null')
             camera.wait_recording(1)
-            camera.stop_recording()
-            
+            camera.stop_recording()            
             while True:
                 camera.capture(stream, 'bgr', use_video_port=True)
                 frame = stream.array
                 cr, cv, cm = detectar_color(frame)
-                
                 return cr, cv, cm
-                
-                stream.seek(0)
-                stream.truncate()
-
-
-
 
 
 def main():
@@ -71,10 +49,5 @@ def main():
     print("Coordenadas del centroide Rojo:", cr)
     print("Coordenadas del centroide Verde:", cv)
     print("Coordenadas del centroide Magenta:", cm)
-
-if __name__ == "__main__":
-  try:  
-    while True:
-      main()
-  except KeyboardInterrupt:
-    pass
+while True:
+    main()
