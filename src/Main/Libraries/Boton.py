18ccf8e27590323071ed446d8cff31eb1274c6a9
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-06-08 15:40:32 +0200

    Update Boton.py

--- src/Main/Libraries/Boton.py
+++ src/Main/Libraries/Boton.py
@@ -1,1 +1,9 @@
-import 
+import RPi.GPIO as GPIO
+
+def button_state():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(9, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    return GPIO.input(9) == GPIO.LOW
+
+while True:
+  print(button_state())
