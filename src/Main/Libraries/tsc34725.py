d16a553cd539f7118c65a85ffa064472f1bc15a8
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-06-08 14:48:37 +0200

    Update tsc34725.py

--- src/Main/Libraries/tsc34725.py
+++ src/Main/Libraries/tsc34725.py
@@ -1,33 +1,60 @@
+import smbus
 import time
-import board
-import busio
-import adafruit_tcs34725
 
-# Configurar el bus I2C
-i2c = busio.I2C(board.SCL, board.SDA)
+# Dirección del sensor TCS34725
+TCS34725_ADDR = 0x29
 
-# Inicializar el sensor
-sensor = adafruit_tcs34725.TCS34725(i2c)
+# Registro de control
+TCS34725_ENABLE = 0x00
+TCS34725_ATIME = 0x01
+TCS34725_GAIN = 0x0F
+TCS34725_ID = 0x12
+TCS34725_CDATAL = 0x14
+TCS34725_CDATAH = 0x15
+TCS34725_RDATAL = 0x16
+TCS34725_RDATAH = 0x17
+TCS34725_GDATAL = 0x18
+TCS34725_GDATAH = 0x19
+TCS34725_BDATAL = 0x1A
+TCS34725_BDATAH = 0x1B
+
+# Configuración del bus I2C
+bus = smbus.SMBus(1)
+
+def tcs34725_init():
+    # Encender el sensor
+    bus.write_byte_data(TCS34725_ADDR, TCS34725_ENABLE, 0x01)
+    
+    # Configurar el tiempo de integración (ATIME)
+    bus.write_byte_data(TCS34725_ADDR, TCS34725_ATIME, 0xEB) # 600ms
+    
+    # Configurar el rango de ganancia (GAIN)
+    bus.write_byte_data(TCS34725_ADDR, TCS34725_GAIN, 0x01) # 1x
+    
+    time.sleep(0.5) # Esperar para que se estabilice el sensor
 
 def read_color():
-    # Leer los valores de color RGB del sensor
-    r, g, b = sensor.color_rgb_bytes
+    # Leer datos de color
+    data = bus.read_i2c_block_data(TCS34725_ADDR, TCS34725_CDATAL | 0x80, 8)
     
-    # Determinar el color basado en los valores RGB
-    if r > 200 and g > 200 and b > 200:
-        color = "Blanco"
-    elif r > g and r > b:
-        color = "Rojo"
-    elif g > r and g > b:
-        color = "Verde"
-    elif b > r and b > g:
-        color = "Azul"
+    # Convertir datos a valores RGB
+    red = data[1] << 8 | data[0]
+    green = data[3] << 8 | data[2]
+    blue = data[5] << 8 | data[4]
+    
+    # Devolver el color dominante
+    if red > green and red > blue:
+        return "Rojo"
+    elif green > red and green > blue:
+        return "Verde"
+    elif blue > red and blue > green:
+        return "Azul"
     else:
-        color = "Otros"
-    
-    return color
+        return "Otros"
 
 try:
+    tcs34725_init() # Inicializar el sensor
+    
     while True:
         color = read_color()
         print("Color detectado:", color)
