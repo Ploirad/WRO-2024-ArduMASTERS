971a302657f5599f06adf78ae605e77881838729
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-06-08 14:11:46 +0200

    Update clasificador_del_tcs.py

--- src/Main/Libraries/clasificador_del_tcs.py
+++ src/Main/Libraries/clasificador_del_tcs.py
@@ -1,4 +1,13 @@
 import tcs34725 as tcs
 
 while True:
-  tcs.
+  r, g, b, c = tcs.read_color()
+  if r > b and r > g and r > 100:
+    print("NARANJA")
+  elif b > r and b > g and b > 100:
+    print("AZUL")
+  elif b > 100 and g > 100 and r > 100:
+    print("GRIS")
+  else:
+    print("---")
+  print(f"r = {r}, g = {g}", b = {b}, c = {c})
