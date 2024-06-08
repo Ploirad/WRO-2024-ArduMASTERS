de7b2ff3d4ebe718f165b92cdbadea4461b4bf74
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-06-08 14:42:45 +0200

    Update tsc34725.py

--- src/Main/Libraries/tsc34725.py
+++ src/Main/Libraries/tsc34725.py
@@ -40,6 +40,7 @@ def read_color():
     return red, green, blue, clear
 
 # Ejemplo de uso
-if __name__ == "__main__":
-    r, g, b, c = read_color()
-    print(f"Rojo: {r}, Verde: {g}, Azul: {b}, Claro: {c}")
+while True:
+    if __name__ == "__main__":
+        r, g, b, c = read_color()
+        print(f"Rojo: {r}, Verde: {g}, Azul: {b}, Claro: {c}")
