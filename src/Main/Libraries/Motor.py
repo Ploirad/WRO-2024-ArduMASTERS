beef8b76fe293179e2b4204dda55dcc301fddbc0
AdriROBOTICS <adriyabe@gmail.com>
2024-06-14 18:37:55 +0200

    update Motor by creating a movement that will be progressive

--- src/Main/Libraries/Motor.py
+++ src/Main/Libraries/Motor.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)
 
@@ -9,30 +10,48 @@ GPIO.setup(3, GPIO.OUT)
 Motor = GPIO.PWM(3, 50)
 Direccion = GPIO.PWM(2, 50)
 
-# Inputs of the function are VELocity and DIRection
+Motor.start(0)
+Direccion.start(7.5)
+
+current_vel = 0
+current_dir = 7.5
+
+def set_servo_angle(servo, angle):
+    duty = angle / 18 + 2.5
+    servo.ChangeDutyCycle(duty)
+
+def smooth_transition(target_vel, target_dir, stop, steps=10, delay=0.1):
+    global current_vel, current_dir
+    if stop:
+        target_vel = 0
+
+    vel_step = (target_vel - current_vel) / steps
+    dir_step = (target_dir - current_dir) / steps
+
+    for i in range(steps):
+        current_vel += vel_step
+        current_dir += dir_step
+        Motor.ChangeDutyCycle(current_vel)
+        Direccion.ChangeDutyCycle(current_dir)
+        time.sleep(delay)
+
 def movement(vel, dir, stop):
     # Convert input into usable value
     if vel == 1:
-        # Max velocity forward 
-        vel = 12.5
-    elif vel == -1: 
-        # Max velocity backward
-        vel = 2.5
+        target_vel = 12.5
+    elif vel == -1:
+        target_vel = 2.5
     else:
-        # If null is introduced stop car
-        stop = 1
+        target_vel = 7.5
 
-    if not stop:
-        Motor.start(vel)
-        if dir == 0:
-            Direccion.start(6.8)
-        elif dir <= -1:
-            Direccion.start(12.5)
-        else:
-            Direccion.start(2.5)
+    if dir == 0:
+        target_dir = 6.8
+    elif dir <= -1:
+        target_dir = 12.5
     else:
-        Motor.stop()
-        Direccion.start(7.5)
+        target_dir = 2.5
+
+    smooth_transition(target_vel, target_dir, stop)
 
 def avance():
-    Motor.start(12.5)
+    Motor.ChangeDutyCycle(12.5)
