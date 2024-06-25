38e936d04fed2169d5721b046d5bd152036ef32a
AdriROBOTICS <adriyabe@gmail.com>
2024-06-25 09:35:38 +0200

    update Motor

--- src/Main/Libraries/Motor.py
+++ src/Main/Libraries/Motor.py
@@ -32,6 +32,52 @@ def movement(vel, dir, stop):
         d = ((0.7 * pow(dir, 2)) - (5 * dir) + 6.8)
         Direccion.start(d)
 
-#This function is used to move the car only forward
-def avance():
-    Motor.start(12.5)
+# -100% a 100%
+def move(percent):
+    if percent > 0:
+        print("AVANCE")
+        MA1.ChangeDutyCycle(0)
+        MA2.ChangeDutyCycle(percent)
+        MB1.ChangeDutyCycle(0)
+        MB2.ChangeDutyCycle(percent)
+
+    elif percent < 0:
+        print("RETROCESO")
+        MA1.ChangeDutyCycle(abs(percent))
+        MA2.ChangeDutyCycle(0)
+        MB1.ChangeDutyCycle(abs(percent))
+        MB2.ChangeDutyCycle(0)
+
+    else:
+        print("STOP")
+        MA1.ChangeDutyCycle(0)
+        MA2.ChangeDutyCycle(0)
+        MB1.ChangeDutyCycle(0)
+        MB2.ChangeDutyCycle(0)
+
+p1MA = 12
+p2MA = 32
+p1MB = 33
+p2MB = 35
+cicloTrabajo = 0
+frecuencia = 1000
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(p1MA, GPIO.OUT)
+GPIO.setup(p2MA, GPIO.OUT)
+GPIO.setup(p1MB, GPIO.OUT)
+GPIO.setup(p2MB, GPIO.OUT)
+
+MA1 = GPIO.PWM(p1MA, frecuencia)
+MA2 = GPIO.PWM(p2MA, frecuencia)
+MB1 = GPIO.PWM(p1MB, frecuencia)
+MB2 = GPIO.PWM(p2MB, frecuencia)
+
+MA1.start(cicloTrabajo)
+MA2.start(cicloTrabajo)
+MB1.start(cicloTrabajo)
+MB2.start(cicloTrabajo)
+
+move(int(input("Percent: ")))
