756d6789cc793445eb729257a478944795173f43
AdriROBOTICS <adriyabe@gmail.com>
2024-06-28 15:15:05 +0200

    update MOTOR_DRIVER

--- src/Main/Libraries/MOTOR_DRIVER.py
+++ src/Main/Libraries/MOTOR_DRIVER.py
@@ -44,7 +44,8 @@ def move(percent_vel, percent_dir):
         # Ajusta aquí la fórmula para el servo
         # 'd' debe estar en el rango adecuado para tu servo
         last_direction = percent_dir
-        center = float(input("center: "))
+        #center = float(input("center: "))
+        center = 8
         d = (((7-center)/10000)*pow(percent_dir, 2))+((1/20)*percent_dir)+center
         Direccion.start(d)
             # 2-12, 9.5
