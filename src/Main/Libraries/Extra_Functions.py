d47bdd845051d27f489b6c4b59524bbadb343400
AdriROBOTICS <adriyabe@gmail.com>
2024-07-07 16:35:00 +0200

    update Extra_Functions

--- src/Main/Libraries/Extra_Functions.py
+++ src/Main/Libraries/Extra_Functions.py
@@ -3,10 +3,12 @@ from Libraries import MOTOR_DRIVER as MD           # MD.move(percent_vel, percen
 from Libraries import Ultrasonidos as HC
 
 def backward(traction, initial_direction):
+    print("BACKWARD STARTED")
     traction = abs(traction)
     front_distance = HC.measure_distance(1)
-    while front_distance < 5:
+    while front_distance < 30:
         print(f"bkwrd: t:{-traction} and d:{-initial_direction}")
         MD.move(-traction, -initial_direction)
         front_distance = HC.measure_distance(1)
     MD.move(traction, initial_direction)
+    print("BACKWARD ENDED")
