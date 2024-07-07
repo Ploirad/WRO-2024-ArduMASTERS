90c12f5a4b77c608de63688bdbf4bdbdf810b8a7
AdriROBOTICS <adriyabe@gmail.com>
2024-07-07 16:38:37 +0200

    update Extra_Functions

--- src/Main/Libraries/Extra_Functions.py
+++ src/Main/Libraries/Extra_Functions.py
@@ -6,7 +6,7 @@ def backward(traction, initial_direction):
     print("BACKWARD STARTED")
     traction = abs(traction)
     front_distance = HC.measure_distance(1)
-    while front_distance < 30:
+    while front_distance < 25:
         print(f"bkwrd: t:{-traction} and d:{-initial_direction}")
         MD.move(-traction, -initial_direction)
         front_distance = HC.measure_distance(1)
