46f85bad7329dcc0bbb651ab2b992359d411f913
AdriROBOTICS <adriyabe@gmail.com>
2024-07-11 22:53:57 +0200

    update Read_UltraSonic_sensors

--- src/Main/Libraries/Read_UltraSonic_sensors.py
+++ src/Main/Libraries/Read_UltraSonic_sensors.py
@@ -2,6 +2,6 @@ while True:
     for i in range(3):
         file_path = f"/tmp/sensor_{i}.txt"
         with open(file_path, "r") as file:
-            content = float(file.read())
+            content = file.read()
             print(f"content{i}: {content}") 
     print("")
