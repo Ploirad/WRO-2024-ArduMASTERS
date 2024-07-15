71925cbf21208b1a0242dc209636e9b69f69bf1f
AdriROBOTICS <adriyabe@gmail.com>
2024-07-15 18:34:19 +0200

    update Execute_MAIN

--- src/Main/Execute_MAIN.py
+++ src/Main/Execute_MAIN.py
@@ -2,7 +2,6 @@
 
 # We import the libraries
 from Libraries.External_Libraries import subprocess
-from Libraries.External_Libraries import time
 
 # We define the commands
 cmd1 = "python3 Libraries/Write_with_UltraSonic_sensors.py"
@@ -10,9 +9,6 @@ cmd2 = "python3 MAIN.py"
 
 # We execute the first command in background
 proc1 = subprocess.Popen(cmd1, shell=True)
-
-# We rest 1 second for stabilizate the sensors
-time.sleep(1)
 
 # Finnaly we execute the second command
 proc2 = subprocess.run(cmd2, shell=True)
