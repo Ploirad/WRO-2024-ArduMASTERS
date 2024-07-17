2e1e7849c839b74b41e3bb36ae66febd00c7a0d5
AdriROBOTICS <adriyabe@gmail.com>
2024-07-17 13:18:01 +0200

    update parking

--- src/Main/Libraries/parking.py
+++ src/Main/Libraries/parking.py
@@ -14,7 +14,6 @@ import MOTOR_DRIVER as MD                # MD.move(percent_vel, percent_dir)
 import Read_UltraSonic_sensors as RHC    # RHC.read_HC(i); 0/1/2/3 = FD/RD/BD/LD
 import New_color_detector as CAM         # CAM.detect_green(frame)    CAM.detect_red(frame)   CAM.detect_magenta(frame)
 import tsc34725 as tcs                   # get_color()
-import parking as P                      # parking()
 import Extra_Functions as F              # backward(traction, initial_direction)
 
 # Initialize the camera as a picamera
