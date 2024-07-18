86722edf2c6494cae32ab38c1ef88c5f33d03a5c
AdriROBOTICS <adriyabe@gmail.com>
2024-07-18 20:04:07 +0200

    Update MAIN and parking and delete Our_Libraries

--- src/Main/Libraries/parking.py
+++ src/Main/Libraries/parking.py
@@ -2,7 +2,12 @@
 
 # Imort the libraries
 from Libraries.External_Libraries import *
-from Libraries.Our_Libraries import *
+import Boton as B                        # B.button_state()
+import MOTOR_DRIVER as MD                # MD.move(percent_vel, percent_dir)
+import Read_UltraSonic_sensors as RHC    # RHC.read_HC(i); 0/1/2/3 = FD/RD/BD/LD
+import New_color_detector as CAM         # CAM.detect_green(frame)    CAM.detect_red(frame)   CAM.detect_magenta(frame)
+import tsc34725 as tcs                   # get_color()
+import Extra_Functions as F              # backward(traction, initial_direction)
 
 # We start counting the time in that we do the race
 started_time = time.time()
