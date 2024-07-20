88b860e2b85cd6deff49f27ae71e07430a671b97
AdriROBOTICS <148376205+AdriROBOTICS@users.noreply.github.com>
2024-07-20 14:36:30 +0200

    Update Extra_Functions.py

--- src/Main/Libraries/Extra_Functions.py
+++ src/Main/Libraries/Extra_Functions.py
@@ -17,12 +17,13 @@ def backward(traction, initial_direction):
 
 # This function is for turn 180 degrees the car
 def change_direction():
+    normal_traction = 30
     print("Backward and right")
-    MD.move(-100, 25)
+    MD.move(-100, normal_traction)
     print("delay 5s")
     time.sleep(1.5)
     print("Forward and left")
-    MD.move(100, -25)
+    MD.move(100, -normal_traction)
     print("delay 2s")
     time.sleep(1.5)
     print("Direction changed")
