6bd2927e1bf03b659518589133cd038765808c40
AdriROBOTICS <adriyabe@gmail.com>
2024-07-22 11:31:01 +0200

    update Extra_Functions

--- src/Main/Libraries/Extra_Functions.py
+++ src/Main/Libraries/Extra_Functions.py
@@ -11,28 +11,27 @@ def backward(traction, initial_direction):
     right_distance = RHC.read_HC(1)
     left_distance = RHC.read_HC(3)
     while front_distance < 30:
-        print(f"bkwrd: t:{-traction} and d:{-initial_direction}")
         if right_distance > left_distance:
             direction = -100
         else:
             direction = 100
-        MD.move(-traction, direction)
+        MD.move(-traction, -direction)
         front_distance = RHC.read_HC(0)
         right_distance = RHC.read_HC(1)
         left_distance = RHC.read_HC(3)
 
-    MD.move(traction, 100)
+    MD.move(traction, direction)
     time.sleep(1)
-    while True:
-        front_distance = RHC.read_HC(0)
-        right_distance = RHC.read_HC(1)
-        left_distance = RHC.read_HC(3)
-        if right_distance <= 85 and left_distance <= 85:
-            break
-        elif right_distance > 86:
-            MD.move(traction, -100)
-        else:
-            MD.move(traction, 100)
+    # while True:
+    #     front_distance = RHC.read_HC(0)
+    #     right_distance = RHC.read_HC(1)
+    #     left_distance = RHC.read_HC(3)
+    #     if right_distance <= 85 and left_distance <= 85:
+    #         break
+    #     elif right_distance > 86:
+    #         MD.move(traction, -100)
+    #     else:
+    #         MD.move(traction, 100)
     MD.move(traction, 0)
     print("BACKWARD ENDED")
 
