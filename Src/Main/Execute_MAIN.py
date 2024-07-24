07e519f2161a5bc45866a6b03c961c9cee7f3f1f
AdriROBOTICS <adriyabe@gmail.com>
2024-07-24 12:30:07 +0200

    update Execute_MAIN

--- Src/Main/Execute_MAIN.py
+++ Src/Main/Execute_MAIN.py
@@ -9,7 +9,7 @@ cmd1 = "python3 Movement.py"
 cmd2 = "python3 MAIN.py"
 
 # We execute the first command in background
-proc1 = subprocess.run(cmd1, shell=True)
+proc1 = subprocess.Popen(cmd1, shell=True)
 
 # Finnaly we execute the second command
 proc2 = subprocess.run(cmd2, shell=True)
