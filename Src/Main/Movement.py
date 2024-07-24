82efd3b6764bdb404a97a3bc8407027b3af84fbf
AdriROBOTICS <adriyabe@gmail.com>
2024-07-24 12:17:13 +0200

    update Movement

--- Src/Main/Movement.py
+++ Src/Main/Movement.py
@@ -18,26 +18,6 @@ for i in range(4):
     GPIO.setup(TRIG[i], GPIO.OUT)
     GPIO.setup(ECHO[i], GPIO.IN)
 
-# This function is for read the distances of the pins specified
-def read_sensor_distance(trig, echo):
-    GPIO.output(trig, False)
-    pulse_start = time.time()
-    while True:
-        GPIO.output(trig, True)
-        time.sleep(0.00001)
-        GPIO.output(trig, False)
-
-        while GPIO.input(echo) == 0:
-            pulse_start = time.time()
-
-        while GPIO.input(echo) == 1:
-            pulse_end = time.time()
-
-        pulse_duration = pulse_end - pulse_start
-        distance = pulse_duration * 17150
-        distance = round(distance, 2)
-        yield distance
-
 # Handler for SIGINT to stop threads
 def signal_handler(sig, frame):
     global stop_event
@@ -69,28 +49,49 @@ def principal_logic(HC0, HC1, HC3):
 
     return traction, direction
 
+def measure_distance(GPIO_TRIGGER, GPIO_ECHO):
+    # Assure the TRIG pin is clean
+    GPIO.output(GPIO_TRIGGER, False)
+    time.sleep(0.000002)  # Reduce the delay here if possible
+
+    # Send a pulse of 10µs to trig the sensor
+    GPIO.output(GPIO_TRIGGER, True)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    # Save the start and end time of the pulse
+    start_time, stop_time = time.time(), time.time()
+    while GPIO.input(GPIO_ECHO) == 0:
+        start_time = time.time()
+
+    while GPIO.input(GPIO_ECHO) == 1:
+        stop_time = time.time()
+
+    # Calculate the duration of the pulse
+    elapsed_time = stop_time - start_time
+
+    # The velocity of sound is 34300 cm/s
+    distance = (elapsed_time * 34300) / 2
+
+    return distance
+
 def move(stop_event):
     while True:
         if stop_event.is_set():
-            return None
+            break
 
-        HC0 = read_sensor_distance(TRIG[0], ECHO[0])
-        HC1 = read_sensor_distance(TRIG[1], ECHO[1])
-        HC2 = read_sensor_distance(TRIG[2], ECHO[2])
-        HC3 = read_sensor_distance(TRIG[3], ECHO[3])
-
-        hc0_distance = next(HC0)
-        hc1_distance = next(HC1)
-        hc2_distance = next(HC2)
-        hc3_distance = next(HC3)
+        HC0 = measure_distance(TRIG[0], ECHO[0])
+        HC1 = measure_distance(TRIG[1], ECHO[1])
+        HC2 = measure_distance(TRIG[2], ECHO[2])
+        HC3 = measure_distance(TRIG[3], ECHO[3])
 
         with open("Move.json", "w", encoding='utf-8') as j:
-            t, d = principal_logic(hc0_distance, hc1_distance, hc3_distance)
+            t, d = principal_logic(HC0, HC1, HC3)
             data = {
-                "HC0": hc0_distance,
-                "HC1": hc1_distance,
-                "HC2": hc2_distance,
-                "HC3": hc3_distance,
+                "HC0": HC0,
+                "HC1": HC1,
+                "HC2": HC2,
+                "HC3": HC3,
                 "TRACTION": t,
                 "DIRECTION": d
             }
