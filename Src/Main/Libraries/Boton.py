f6eb16d6308bf2aff8857908332f17f9eb0779b8
CreatorIsTheBest <148372371+CreatorIsTheBest@users.noreply.github.com>
2024-09-02 11:40:56 +0200

    Add files via upload

---
+++ Src/Main/Libraries/Boton.py
@@ -0,0 +1,10 @@
+# This code is a library for the button to know if the button has been pressed or not
+
+# Libraries
+import RPi.GPIO as GPIO
+
+# This function is used to give a boolean variable if the button has pressed (True) or not (False)
+def button_state():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(9, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    return GPIO.input(9) == GPIO.HIGH
