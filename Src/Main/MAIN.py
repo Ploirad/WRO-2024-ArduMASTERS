bb279348cfc1541c8cb23bbfaca768d3dc4baa89
CreatorIsTheBest <148372371+CreatorIsTheBest@users.noreply.github.com>
2024-09-03 13:53:07 +0200

    Add files via upload

--- Src/Main/MAIN.py
+++ Src/Main/MAIN.py
@@ -30,6 +30,7 @@ first_right_distance = 0
 first_loop_done = False
 second_round = False
 tim = 1.5
+print("variables inizialiced")
 
 def main_logic(areas):
     max_area = max(areas, key=areas.get)
@@ -49,123 +50,126 @@ def main_logic(areas):
     return tract, direction, max_area, ignore
 
 park = False
-for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
-    image = frame.array
-    height, width = image.shape[:2]
-    lower_half = image[height//2:, :]
-    _, green_area = cam.detect_green(lower_half)
-    _, red_area = cam.detect_red(lower_half)
-    magenta_centroid, magenta_area = cam.detect_magenta(lower_half)
+try:
+    for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
+        image = frame.array
+        height, width = image.shape[:2]
+        lower_half = image[height//2:, :]
+        _, green_area = cam.detect_green(lower_half)
+        _, red_area = cam.detect_red(lower_half)
+        magenta_centroid, magenta_area = cam.detect_magenta(lower_half)
 
-    color_areas = {"green": green_area, "red": red_area, "magenta": magenta_area}
+        color_areas = {"green": green_area, "red": red_area, "magenta": magenta_area}
 
-    t, d, color, ignore = main_logic(color_areas)
-    
-    if color == "magenta":
-        park = True
-    else:
-        park = False
-
-    CAM = {
-            "Ignore": ignore,
-            "Color": color,
-            "Parking": park,
-            "TRACTION": t,
-            "DIRECTION": d,
-            "MagentaC": magenta_centroid,
-            "GArea": green_area,
-            "RArea": red_area,
-            "MArea": magenta_area
-        }
-
-    try:
-        if can_start:
-            if not first_loop_done:
-                with open("Libraries/Json/Distance.json", "r", encoding='utf-8') as f:
-                    Move = json.load(f)
-                    first_front_distance = Move["HC0"]
-                    first_right_distance = Move["HC1"]
-                first_loop_done = True
-
-            traction = 0
-            direction = 0
-
-            if (color == "red" or color == "green" or color == "magenta") and not ignore:
-                second_round = True
-
-            if second_round and possible_changing_direction and not ignore:
-                last_pillar = color
-
-            if waiting_magenta:
-                if second_round:
-                    if color == "magenta":
-                        End.parking()
-                else:
-                    if tcs_color == "Gray":
-                        End.home_sweet_home(first_front_distance, first_right_distance)
-                        break
-
-            if ("TRACTION" in CAM and "DIRECTION" in CAM) and not CAM["Ignore"]:
-                traction = int(CAM["TRACTION"])
-                direction = int(CAM["DIRECTION"])
-            
-            else:
-                if CAM["Ignore"]:
-                    print("Ignore CAM")
-                    with open("Libraries/Json/Move.json", "r", encoding='utf-8') as f:
-                        Move = json.load(f)
-                        print(Move)
-
-                        if "TRACTION" in Move and "DIRECTION" in Move:
-                            traction = int(Move["TRACTION"])
-                            direction = int(Move["DIRECTION"])
-
-                        else:
-                            print("Invalid data format in JSON file")
-
-            with open("Libraries/Json/tcs_color_detection.json", "r", encoding='utf-8') as f:
-                tcs = json.load(f)
-                print(tcs)
-
-                laps = tcs["laps"]
-                tcs_first_color = tcs["first_color_obteined"]
-                turns = tcs["turns"]
-                tcs_color = tcs["color_obteined"]
-
-                if laps == 1 and turns == 3:
-                    possible_changing_direction = True
-
-                if tcs_first_color == tcs_color and possible_changing_direction:
-                    if last_pillar == "red":
-                        F.change_direction()
-                    possible_changing_direction = False
-                    TCS_M.extra_lap = True
-
-                if laps >= 3:
-                    print("OK")
-                    waiting_magenta = True
-
-                with open("Libraries/Json/Movement.json", "w") as j:
-                    data_to_write = {
-                        "traccion": traction,
-                        "direccion": direction
-                    }
-                    json.dump(data_to_write, j, indent=4)
-
-
-                if traction < 0:
-                    time.sleep(tim)
-
+        t, d, color, ignore = main_logic(color_areas)
+        
+        if color == "magenta":
+            park = True
         else:
-            print("Waiting for start signal")
-            if Boton.button_state():
-                can_start = True
+            park = False
 
-    except FileNotFoundError:
-        print("Move.json file not found. Make sure the file exists.")
-    except json.JSONDecodeError:
-        print("Error decoding JSON. Ensure the JSON format is correct.")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        CAM = {
+                "Ignore": ignore,
+                "Color": color,
+                "Parking": park,
+                "TRACTION": t,
+                "DIRECTION": d,
+                "MagentaC": magenta_centroid,
+                "GArea": green_area,
+                "RArea": red_area,
+                "MArea": magenta_area
+            }
 
-    raw_capture.truncate(0)
+        try:
+            if can_start:
+                if not first_loop_done:
+                    with open("Libraries/Json/Distance.json", "r", encoding='utf-8') as f:
+                        Move = json.load(f)
+                        first_front_distance = Move["HC0"]
+                        first_right_distance = Move["HC1"]
+                    first_loop_done = True
+
+                traction = 0
+                direction = 0
+
+                if (color == "red" or color == "green" or color == "magenta") and not ignore:
+                    second_round = True
+
+                if second_round and possible_changing_direction and not ignore:
+                    last_pillar = color
+
+                if waiting_magenta:
+                    if second_round:
+                        if color == "magenta":
+                            End.parking()
+                    else:
+                        if tcs_color == "Gray":
+                            End.home_sweet_home(first_front_distance, first_right_distance)
+                            break
+
+                if ("TRACTION" in CAM and "DIRECTION" in CAM) and not CAM["Ignore"]:
+                    traction = int(CAM["TRACTION"])
+                    direction = int(CAM["DIRECTION"])
+                
+                else:
+                    if CAM["Ignore"]:
+                        print("Ignore CAM")
+                        with open("Libraries/Json/Move.json", "r", encoding='utf-8') as f:
+                            Move = json.load(f)
+                            print(Move)
+
+                            if "TRACTION" in Move and "DIRECTION" in Move:
+                                traction = int(Move["TRACTION"])
+                                direction = int(Move["DIRECTION"])
+
+                            else:
+                                print("Invalid data format in JSON file")
+
+                with open("Libraries/Json/tcs_color_detection.json", "r", encoding='utf-8') as f:
+                    tcs = json.load(f)
+                    print(tcs)
+
+                    laps = tcs["laps"]
+                    tcs_first_color = tcs["first_color_obteined"]
+                    turns = tcs["turns"]
+                    tcs_color = tcs["color_obteined"]
+
+                    if laps == 1 and turns == 3:
+                        possible_changing_direction = True
+
+                    if tcs_first_color == tcs_color and possible_changing_direction:
+                        if last_pillar == "red":
+                            F.change_direction()
+                        possible_changing_direction = False
+                        TCS_M.extra_lap = True
+
+                    if laps >= 3:
+                        print("OK")
+                        waiting_magenta = True
+
+                    with open("Libraries/Json/Movement.json", "w") as j:
+                        data_to_write = {
+                            "traccion": traction,
+                            "direccion": direction
+                        }
+                        json.dump(data_to_write, j, indent=4)
+
+
+                    if traction < 0:
+                        time.sleep(tim)
+
+            else:
+                print("Waiting for start signal")
+                if Boton.button_state():
+                    can_start = True
+
+        except FileNotFoundError:
+            print("Move.json file not found. Make sure the file exists.")
+        except json.JSONDecodeError:
+            print("Error decoding JSON. Ensure the JSON format is correct.")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
+        raw_capture.truncate(0)
+except:
+    print("no frame") 
