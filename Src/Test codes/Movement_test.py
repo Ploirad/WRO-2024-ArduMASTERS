f19c9fc69ed666a580f372da54f270e56dc68382
Ploirad <dlopezp36@gmail.com>
2024-09-04 10:11:03 +0200

    Movement_test changed

--- Src/Test codes/Movement_test.py
+++ Src/Test codes/Movement_test.py
@@ -2,7 +2,9 @@ import time
 from Libraries import MOTOR_DRIVER as MD
 
 while True:
-    vel = float(input("PERCENT VEL: "))
+    vel_r = float(input("PERCENT VEL FOR THE RIGHT: "))
+    vel_l = float (input("PERCENT VEL FOR THE LEFT: "))
+    vel = (vel_l, vel_r)
     dir = float(input("PERCENT DIR: "))
     MD.move(vel, dir)
     time.sleep(4)
