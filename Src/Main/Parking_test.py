b0e46e55528c85a9b278c5f49a937ec238d69aec
CreatorIsTheBest <148372371+CreatorIsTheBest@users.noreply.github.com>
2024-09-10 18:21:13 +0200

    Update Parking_test.py

--- Src/Main/Parking_test.py
+++ Src/Main/Parking_test.py
@@ -4,7 +4,7 @@ import json
 while True:
   with open ("Libraries/Json/CAM.json", "r", encoding = "utf-8") as d:
     data = json.load(d)
-    M = d["MagentaC"]
+    M = data["MagentaC"]
     if M != None:
       ER.parking(False)
       Break
