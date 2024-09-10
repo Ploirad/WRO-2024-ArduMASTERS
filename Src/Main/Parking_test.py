5e43c5e69e6061f49dd708a78a0a825a8f0407dd
CreatorIsTheBest <148372371+CreatorIsTheBest@users.noreply.github.com>
2024-09-10 18:23:08 +0200

    Update Parking_test.py

--- Src/Main/Parking_test.py
+++ Src/Main/Parking_test.py
@@ -1,8 +1,9 @@
 import Libraries.End_rounds as ER
 import json
+import os
 
 while True:
-  with open ("Libraries/Json/CAM.json", "r", encoding = "utf-8") as d:
+  with open (os.path.join(os.path.dirname(__file__), "Json", "CAM.json"), "r", encoding = "utf-8") as d:
     data = json.load(d)
     M = data["MagentaC"]
     if M != None:
