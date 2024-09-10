68eadb2a6bffa3a956768e421558252bbe377343
CreatorIsTheBest <148372371+CreatorIsTheBest@users.noreply.github.com>
2024-09-10 18:39:04 +0200

    Update Parking_test.py

--- Src/Main/Parking_test.py
+++ Src/Main/Parking_test.py
@@ -1,13 +1,15 @@
 import Libraries.End_rounds as ER
 import json
 import os
+import time
 
 while True:
   with open(os.path.join(os.path.dirname(__file__), "Libraries", "Json", "CAM.json"), "r", encoding = "utf-8") as d:
     data = json.load(d)
     M = data["MagentaC"]
     print(M)
-    if M != None:
-      print("Imma park")
-      ER.parking(False)
-      break
+  if M != None:
+    print("Imma park")
+    ER.parking(False)
+    break
+  time.sleep(100)  
