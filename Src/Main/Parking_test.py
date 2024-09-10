d9d718f5984c689ab67027045b4a95fe2cfc0bab
CreatorIsTheBest <148372371+CreatorIsTheBest@users.noreply.github.com>
2024-09-10 18:17:17 +0200

    Update Parking_test.py

--- Src/Main/Parking_test.py
+++ Src/Main/Parking_test.py
@@ -1,3 +1,9 @@
 import Libraries.End_rounds as ER
+import json
 
-ER.parking(False)
+while True:
+  with open ("Libraries/Json/CAM.json", "r", encoding = "utf-8") as d:
+    data = json.load(d)
+    M = d["MagentaC"]
+    if M not None:
+      ER.parking(False)
