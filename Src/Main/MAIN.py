79b78f7e5825b6b24d91bc0e8c97fa5a17716bac
CreatorIsTheBest <148372371+CreatorIsTheBest@users.noreply.github.com>
2024-09-15 09:47:58 +0200

    Revisión MAIN.py mainV3

--- Src/Main/MAIN.py
+++ Src/Main/MAIN.py
@@ -1,4 +1,4 @@
-import json
+ import json
 import time
 from Libraries import MOTOR_DRIVER as MD
 from Libraries import Boton as B
@@ -6,22 +6,9 @@ import Libraries.Movement_Functions as F
 import Libraries.End_rounds as End
 import math as M
 
-Phase = 0 # initial phase, unknown direcction
-direction = ""
-alpha = 30 # Car turning angle
-phase = 0
 button_pressed = False
 time_to_90_degrees = 5
-color_has_been_detected = False
-
-def TD(): # turn distance
-    return (M.pi * (11 / M.sin(alpha)))/2
-
-def DFL(): # distance to first line
-    return M.tan(alpha) * Wall_dist
-
-def AD(): # aproximation distance
-    return Wall_dist - TD()
+half_turn = False
 
 if __name__ == "__main__":
     while True:
@@ -45,8 +32,8 @@ if __name__ == "__main__":
             with open("Libraries/Json/tsc_color_detection.json", "r", encoding="utf-8") as T:
                 tcs_data = json.load(T)
 
-                direction = tcs_data["first_color_obteined"]
-                Color = tcs_data["color_obteined"]
+                orientation = tcs_data["first_color_obteined"]
+                floor_color = tcs_data["color_obteined"]
                 turns = tcs_data["turns"]
                 laps = tcs_data["laps"]
 
@@ -58,53 +45,40 @@ if __name__ == "__main__":
                 HC2 = HC_data["HC2"]
                 HC3 = HC_data["HC3"]
 
-                if direction == "blue":
-                    Wall_dist = 95 - HC1
-                    print("Anticlockways direccion")
-                elif direction == "orange":
-                    Wall_dist = 95 - HC3
-                    print("Clockways direccion")
-                else:
-                    print("Unknown direcction") 
-    
-            if ignore == True:
-                if round == 3:
-                    if color_has_been_detected:
-                        if parking == True:
-                            End.parking()
+            if laps == 1 and  turns == 3 and orientation == floor_color:
+                if last_sign == "red":
+                    F.change_direction()
+                    half_turn = True
+            
+            if laps >= 3:
+                    if parking == True:
+                        End.parking(half_turn)
                     else:
                         End.home_sweet_home()
 
-                if Color == "unknown" or Color == "gray":
-                    if HC1+HC3 > ((HC1+HC3)/2)-15:
-                        # If there is more distance to the right than the left
-                        if HC1 > HC3: 
-                            prev_front = HC0 # Save distance to front walls
-                            MD.move(25, 100) # Turn to right until the front walls it's at the left
-                            while not HC3 == range(prev_front-30,prev_front):  time.sleep(0.25)
-                            MD.move(25, 0) # Continue forward until abandoning the corner
-                            while HC1 + HC3 >= 150: time.sleep(0.25)
-                            turns += 1
+            if ignore == True:
 
-                        # If there is more distance to the left than the right
-                        elif HC1 < HC3: 
-                            prev_front = HC0 # Save distance to front walls
-                            MD.move(25, -100) # Turn to right until the front walls it's at the right
-                            while not HC3 <= prev_front:  time.sleep(0.25)
-                            MD.move(25, 0) # Continue forward until abandoning the corner
-                            while HC1 + HC3 >= 150: time.sleep(0.25)
+                if floor_color == "unknown" or floor_color == "gray":
+                    
+                    if HC1 <= (HC1 + HC3)/2 - 10:
+                        MD.move(25,-100)
+                    if HC3 <= (HC1 + HC3)/2 - 10:
+                        MD.move(25,100)
+                    else:
+                        MD.move(25,0)
+                
                 else:
-                    if Color == "blue":
+                    if floor_color == "blue":
                         MD.move(25, -100)
                         time.sleep(time_to_90_degrees)
-                        color_has_been_detected = True
-                    elif Color == "orange":
+                
+                    elif floor_color == "orange":
                         MD.move(25, 100)
                         time.sleep(time_to_90_degrees)
-                        color_has_been_detected = True
+
             else:
                 if color == "green" or color== "red":
-                    F.pivot_aproximation(color)
+                    last_sign = F.pivot_aproximation(color)
         else:
             if B.button_state() == True:
                 print("Button pressed")
